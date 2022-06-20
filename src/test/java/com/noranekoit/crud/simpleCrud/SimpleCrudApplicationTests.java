87aaf82ca436bc82804af2e9b9e53f10e83e3fe6
NoraNekoIT <bunsmart18@gmail.com>
2022-06-20 12:34:06 +0700

    jdk 8 set

--- src/test/java/com/noranekoit/crud/simpleCrud/SimpleCrudApplicationTests.java
+++ src/test/java/com/noranekoit/crud/simpleCrud/SimpleCrudApplicationTests.java
@@ -1,10 +1,15 @@
 package com.noranekoit.crud.simpleCrud;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimpleCrudApplicationTests {
 
+	@Test
+	void testSajaLah(){
+		Assertions.assertEquals(5,5);
+	}
 
 }
